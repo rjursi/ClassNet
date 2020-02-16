305ae438beee7dcb76fb670969aa13a6bce9ae06
YunKwon Kim <dfg3790@gmail.com>
2020-02-16 14:42:07 +0900

    v20200130_client_server

--- monitorSharing_client/monitorSharing_client/main.cpp
+++ monitorSharing_client/monitorSharing_client/main.cpp
@@ -1,12 +1,72 @@
-#include "client_socket_class.h"
+#include <stdio.h>
 
-/*#include<opencv\cv.h>
-#include<opencv\highgui.h>
+#include <curl/curl.h>
+
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
 
 using namespace cv;
 
+struct FtpFile {
+	const char *filename;
+	FILE *stream;
+};
+
+static size_t my_fwrite(void *buffer, size_t size, size_t nmemb, void *stream)
+{
+	struct FtpFile *out = (struct FtpFile *)stream;
+	if (!out->stream) {
+		/* open file for writing */
+		out->stream = fopen(out->filename, "wb");
+		if (!out->stream)
+			return -1; /* failure, can't open file to write */
+	}
+	return fwrite(buffer, size, nmemb, out->stream);
+}
+
+void image_download() {
+	CURL *curl;
+	CURLcode res;
+	struct FtpFile ftpfile = {
+	  "ScreenCapture.jpeg", /* name to store the file as if successful */
+	  NULL
+	};
+
+	curl_global_init(CURL_GLOBAL_DEFAULT);
+
+	curl = curl_easy_init();
+	if (curl) {
+		/*
+		 * You better replace the URL with one that works!
+		 */
+		curl_easy_setopt(curl, CURLOPT_URL,
+			"ftp://192.168.31.218:9999/ScreenCapture.jpeg");
+		/* Define our callback to get called when there's data to be written */
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, my_fwrite);
+		/* Set a pointer to our struct to pass to the callback */
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &ftpfile);
+
+		/* Switch on full protocol/debug output */
+		curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+
+		res = curl_easy_perform(curl);
+
+		/* always cleanup */
+		curl_easy_cleanup(curl);
+
+		if (CURLE_OK != res) {
+			/* we failed */
+			fprintf(stderr, "curl told us %d\n", res);
+		}
+	}
+
+	if (ftpfile.stream)
+		fclose(ftpfile.stream); /* close the local file */
+
+	curl_global_cleanup();
+}
+
 void image_viewer() {
-	::SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_SYSTEM_AWARE);
 	String filename = "ScreenCapture.jpeg";
 
 	Mat origin_img = imread(filename, CV_LOAD_IMAGE_COLOR);
@@ -16,15 +76,14 @@ void image_viewer() {
 	imshow("viewer", resizing_img);
 
 	waitKey(1);
-}*/
+}
 
-int main() {
-	client_socket_class client_socket_object;
-
-	client_socket_object.connect_server();
+int main(void)
+{
 	while (1) {
-		client_socket_object.recvfile();
-		//image_viewer();
+		system("cls");
+		image_download();
+		image_viewer();
 	}
 
 	return 0;
--- monitorSharing_server/monitorSharing_server/main.cpp
+++ monitorSharing_server/monitorSharing_server/main.cpp
@@ -1,11 +1,15 @@
-#include "server_socket_class.h"
-#include <Windows.h>
+#include <iostream>
+#include <atlimage.h>
 #include <gdiplusbitmap.h>
+
+#include <Windows.h>
+
+using namespace std;
 
 void ScreenCapture() {
 	HDC HandleDC = GetWindowDC(NULL);
 	CImage imageObject;
-	
+
 	::SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_SYSTEM_AWARE);
 
 	int cx = GetSystemMetrics(SM_CXSCREEN);
@@ -15,24 +19,23 @@ void ScreenCapture() {
 	imageObject.Create(cx, cy, colorDepth);
 	StretchBlt(imageObject.GetDC(), 0, 0, cx, cy, HandleDC, 0, 0, cx, cy, SRCCOPY);
 
-	imageObject.Save(L"ScreenCapture.jpeg", Gdiplus::ImageFormatJPEG);
+	imageObject.Save(L"C://monitorSharingFTP/ScreenCapture.jpeg", Gdiplus::ImageFormatJPEG);
 
 	ReleaseDC(NULL, HandleDC);
 	imageObject.ReleaseDC();
 }
 
 int main() {
-	server_socket_class server_socket_object;
-
 	char c_input = NULL;
 	cout << "> ";
 	cin >> c_input;
 	if (c_input == 's') {
 		system("cls");
-		server_socket_object.connect_client();
 		while (1) {
 			ScreenCapture();
-			server_socket_object.sendfile();
+			system("cls");
+			cout << "uploading...";
+			Sleep(50);
 		}
 	}
 
