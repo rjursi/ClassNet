fc6f783d4005924966c059cd7aeaeccfc16ce039
KimTaewoo <xass1995@gmail.com>
2020-02-25 18:37:45 +0900

    MONITOR SHARING, Socket Part update.20200222

--- monitorSharing_client/monitorSharing_client/Form1.cs
+++ monitorSharing_client/monitorSharing_client/Form1.cs
@@ -8,39 +8,86 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
 namespace monitorSharing_client
 {
     public partial class Form1 : Form
     {
-        //int fileNum = 0;
+        Socket socketServer;
 
+        Byte[] sendData;
+        int imgSize = 0;
+
+        //Byte[] imgSizeBuffer = new Byte[8];
+        Byte[] imgRecv = new byte[100000];
+
+        delegate void ThreadDelegate(int imgSize, Image img);
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            /*
-            for (int i = 0; i < 10; i++)
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            socketServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("192.168.31.200"), 7979); // 192.168.31.218
+            socketServer.Connect(serverEndPoint);
+
+            new Thread(() => recieveThread(socketServer)).Start();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            socketServer.Close();
+            Dispose();
+        }
+
+        public void recieveThread(Socket socketServer)
+        {
+            while (true)
             {
-                string fileName = "ftp://192.168.31.200:9999/ScreenCapture";
-                string fileType = ".jpeg";
+                sendData = Encoding.UTF8.GetBytes("ready");
+                socketServer.Send(sendData);
 
-                pictureBox1.LoadAsync(@fileName + i + fileType);
+                /*socketServer.Receive(imgSizeBuffer);
+                imgSize = BitConverter.ToInt32(imgSizeBuffer, 0);*/
+
+                socketServer.Receive(imgRecv);
+
+                try
+                {
+                    MemoryStream ms = new MemoryStream(imgRecv);
+                    Image img = Image.FromStream(ms);
+
+                    this.Invoke(new ThreadDelegate(outputDelegate), imgSize, img);
+
+                    ms.Close();
+                }
+                catch
+                {
+
+                }
+                                
+                Array.Clear(imgRecv, 0, imgRecv.Length);
+                //Array.Clear(imgSizeBuffer, 0, imgSizeBuffer.Length);
+
+                Array.Clear(sendData, 0, sendData.Length);
             }
-            */
+        }
 
-            pictureBox1.LoadAsync(@"ftp://192.168.31.200:9999/ScreenCapture.jpeg");
-
-            /*
-            string fileName = "ftp://192.168.31.200:9999/ScreenCapture";
-            string fileType = ".jpeg";
-
-            pictureBox1.LoadAsync(@fileName + fileNum + fileType);
-
-            fileNum++;
-            */
+        public void outputDelegate(int imgSize, Image img)
+        {
+            //label1.Text = imgSize.ToString();
+            pictureBox1.Image = img;
         }
     }
 }
--- monitorSharing_server/monitorSharing_server/Form1.Designer.cs
+++ monitorSharing_server/monitorSharing_server/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace monitorSharing_client
+﻿namespace monitorSharing_server
 {
     partial class Form1
     {
@@ -29,63 +29,48 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 288);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(480, 35);
+            this.button1.Size = new System.Drawing.Size(180, 35);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Connect";
+            this.button1.Text = "시작";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label1
+            // button2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 339);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 15);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Image Size";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(480, 270);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.button2.Location = new System.Drawing.Point(12, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "종료";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(502, 363);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(205, 100);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
-            this.Text = "Client";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Text = "Server";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
 
         }
 
         #endregion
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
