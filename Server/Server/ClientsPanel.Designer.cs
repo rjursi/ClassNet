ed228a6f341dfbc4ed583e4761d58b39979e0e92
platina78 <ghdtjdwns87@gmail.com>
2020-07-16 21:44:23 +0900

    Add ClientsPanel

--- Server/Server/ClientsPanel.Designer.cs
+++ Server/Server/ClientsPanel.Designer.cs
@@ -28,10 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1280, 720);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // ClientsPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "ClientsPanel";
+            this.Size = new System.Drawing.Size(1280, 720);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
