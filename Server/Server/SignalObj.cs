1eabc9f16aec9171cc5265eef8609949f3043afa
KimTaewoo <xass1995@gmail.com>
2020-10-09 00:42:57 +0900

    set receiving client screen for monitoring

--- Server/Server/SignalObj.cs
+++ Server/Server/SignalObj.cs
@@ -11,6 +11,7 @@ namespace Server
             IsLock = false;
             IsInternet = false;
             IsPower = false;
+            IsMonitoring = false;
         }
 
         // 서버가 꺼져있는지, 서버가 컨트롤 중인지는 서버만 제어
@@ -18,6 +19,7 @@ namespace Server
         public bool IsLock { get; set; }
         public bool IsInternet { get; set; }
         public bool IsPower { get; set; }
+        public bool IsMonitoring { get; set; }
 
         // 화면 전송 데이터 getter, setter
         public byte[] ServerScreenData { get; set; } = null;
