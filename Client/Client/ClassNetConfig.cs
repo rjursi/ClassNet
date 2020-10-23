81c7c1f117380be0328196d9b50656abcbd0552e
rjursi <dfg3790@gmail.com>
2020-10-23 15:44:57 +0900

    remove Encrypt

--- Client/Client/ClassNetConfig.cs
+++ Client/Client/ClassNetConfig.cs
@@ -8,87 +8,19 @@ namespace Client
 {
     class ClassNetConfig
     {
-        public static void DoProtection()
-        {
-            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-            ConfigurationSection section = config.AppSettings;
-
-            if (!section.SectionInformation.IsProtected && !section.ElementInformation.IsLocked)
-            {
-                {
-                    section.SectionInformation.ProtectSection("DataProtectionConfigurationProvider");
-                    section.SectionInformation.ForceSave = true;
-                    config.Save(ConfigurationSaveMode.Full);
-
-                    ConfigurationManager.RefreshSection(config.AppSettings.SectionInformation.Name);
-                }
-            }
-        }
+        
 
         public static string GetAppConfig(string key)
         {
-            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-            ConfigurationSection section = config.AppSettings;
-            string result = "";
-
-            try
-            {
-
-
-                if (section != null)
-                {
-                    if (section.SectionInformation.IsProtected)
-                    {
-                        section.SectionInformation.UnprotectSection();
-                        section.SectionInformation.ForceDeclaration(true);
-                        section.SectionInformation.ForceSave = true;
-                        config.Save(ConfigurationSaveMode.Full);
-                    }
-
-                }
-
-                result = ConfigurationManager.AppSettings[key];
-            }
-            catch (ConfigurationErrorsException)
-            {
-
-                DoProtection();
-
-                if (section != null)
-                {
-                    if (section.SectionInformation.IsProtected)
-                    {
-                        section.SectionInformation.UnprotectSection();
-                        section.SectionInformation.ForceDeclaration(true);
-                        section.SectionInformation.ForceSave = true;
-                        config.Save(ConfigurationSaveMode.Full);
-                    }
-                    result = ConfigurationManager.AppSettings[key];
-                }
-
-            }
-            DoProtection();
-
-            return result;
-
+       
+            return ConfigurationManager.AppSettings[key];
+  
         }
 
         public static void SetAppConfig(string key, string value)
         {
             Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
 
-            ConfigurationSection section = config.AppSettings;
-
-            if (section != null)
-            {
-                if (section.SectionInformation.IsProtected)
-                {
-                    section.SectionInformation.UnprotectSection();
-                    section.SectionInformation.ForceDeclaration(true);
-                    section.SectionInformation.ForceSave = true;
-                    config.Save(ConfigurationSaveMode.Full);
-                }
-            }
 
             KeyValueConfigurationCollection cfgCollection = config.AppSettings.Settings;
 
@@ -98,10 +30,7 @@ namespace Client
 
             config.Save(ConfigurationSaveMode.Modified);
 
-            section.SectionInformation.ProtectSection("DataProtectionConfigurationProvider");
-            section.SectionInformation.ForceSave = true;
-            config.Save(ConfigurationSaveMode.Full);
-
+     
             ConfigurationManager.RefreshSection(config.AppSettings.SectionInformation.Name);
         }
     }
